aa71d3bfcc323f9e9b37129e9f803f8fe86c3e21
BlackZu01 <citozuluaga44@gmail.com>
2023-04-22 11:45:49 -0500

    Types added

--- graph.py
+++ graph.py
@@ -3,12 +3,9 @@ import numpy as np
 from multiprocessing import Pool, cpu_count
 import networkx as nx
 import matplotlib.pyplot as plt
-import plotly.graph_objs as go
-import plotly.io as pio
-
 
 class SimplicialSet:
-    def __init__(self, points) -> None:
+    def __init__(self, points: np.ndarray) -> None:
         self.points = points 
         self.graph = {}
 
@@ -20,7 +17,7 @@ class SimplicialSet:
                 edges = r.get()
                 self.graph.update(edges)
 
-    def computeDistances(self, i, points, epsilon):
+    def computeDistances(self, i: int, points: np.ndarray, epsilon: float) -> dict:
         edges = {}
         for k in range(points.shape[0]):
             if i != k:
@@ -28,20 +25,22 @@ class SimplicialSet:
                     edges[str(points[i])] = points[k]
         return edges
     
-    def plotGraph(self):
+    def plotGraph(self) -> None:
         G = nx.Graph()
         for node in self.graph:
             G.add_node(node)
             for neighbor in self.graph[node]:
                 G.add_edge(node, neighbor)
+
         pos = nx.spring_layout(G)
         nx.draw(G, pos, with_labels=False, node_color='lightblue', alpha=0.8, edgecolors='black')
         plt.show()
     
 
 data = np.loadtxt('data/klein_bottle_pointcloud_new_900.txt')
+print(type(data))
 st = SimplicialSet(data)
-st.createEdges(1)
+st.createEdges(epsilon=1)
 st.plotGraph()
 
 # print(st.graph)
